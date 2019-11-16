1a3282b0fa4a70311acb913961bb6f6c503ca149
Trance Holidays <topenion@gmail.com>
2019-11-16 11:58:10 +0530

    "Initial commit"

---
+++ main.go
@@ -0,0 +1,13 @@
+/*This is main package. All Files in same directory must have package main or package main_test
+* Also there must be a main function in any of the main package files, which is the starting
+* point of the program.
+* NOTE: package main makes executables, other packages are just packages to be used in your program*/
+package main
+
+import "fmt" //
+
+var choice int
+
+func main() {
+	fmt.Println("The Main function")
+}
